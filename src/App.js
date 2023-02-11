ff6029dc6909955d97f7d04c7dbd8edc0fa72d32
Taric Folkes <taricdf@gmail.com>
2023-02-11 01:50:53 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ServiceProvider from './Components/pages/serviceProvider'
+import Home from './Components/pages/home'
+import Reviews from './Components/pages/reviews'
+import Signup from './Components/pages/signup'
+import Login from './Components/pages/login'
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/home" component={ Home } />
+          <Route path="/serviceProvider"  component={ServiceProvider} />
+          <Route path="/reviews" component={Reviews} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+        </Routes>
+        <Home />
+        <Footer />
+      </Router>
+      
+    </>
   );
 }
 
---
+++ src/Components/HeroSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <h1>#Placeholder#</h1>
+      <p>#Placeholder#</p>
+      <div>
+        <input placeholder="Enter Post Title"/>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
